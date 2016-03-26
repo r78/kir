65e2e16a6e27c1bb3a85bbfdf36867b6c61bd3bd
r <r@localhost>
2016-03-26 18:15:15 +0100

    new file:   1.10.c 	new file:   1.11.c 	new file:   1.12.c

---
+++ 1.10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* copies its input to its output, replacing each tab by \t,
+ * each backspace by \b, and each backslash by \\ */
+
+main() {
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            putchar('\\');
+            putchar('t');
+        } else if (c == '\b') {
+            putchar('\\');
+            putchar('b');
+        } else if (c == '\\') {
+            putchar('\\');
+            putchar('\\');
+        } else {
+            putchar(c);
+        }
+    }
+}

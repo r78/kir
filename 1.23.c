93cd755c9c43fb9c456fc440c7540f9d4b5237e1
r <r@localhost>
2016-04-06 21:50:50 +0200

    simplify, c comments are /* like this */ 	modified:   1.23.c

--- 1.23.c
+++ 1.23.c
@@ -5,17 +5,19 @@
 
 int get_line(char line[], int maxline);
 void uncomment(char s[], int len);
-void printbuf(char s[], int end);
-void printbuftail(char s[], int start, int len);
+int in_ml, ml_end, eol;
 
 /* removes all coments from a c program 
 don't forget to handle quoted strings and characters properly
 c comments do not nest */
 
-main() {
+int main() {
   int len;
-  char line[MAXLINE];  
-  
+  char line[MAXLINE];
+  extern int in_ml, ml_end, eol;
+  in_ml = FALSE;
+  ml_end = -1;
+
   while ((len = get_line(line, MAXLINE)) > 0) {
     uncomment(line, len);
   }
@@ -27,50 +29,32 @@ void uncomment(char s[], int len) {
   char c;
   int cmt_pos = -1;
   int ml_start = -1;
-  int ml_end = -1;
   int in_cmt = FALSE;
-  int in_ml = FALSE;
+  extern int in_ml, ml_end, eol;
+  eol = FALSE;
   
-
   for (i = 0; (c = s[i]) != '\0'; i++) {
     //TODO check if we are inside quoted text
-    //TODO check wether we are inside a single comment, multiline comment, no comment
-    if (in_ml == FALSE)  {
-      if (in_cmt == FALSE && in_ml == FALSE && c == '/' && s[i+1] == '/') {
-        cmt_pos = i;
-        printf("sl, %d, in? %d, ml? %d\n", i, in_cmt, in_ml);
-        printbuf(s, i);
-        in_cmt = TRUE;
-      } else if (in_cmt == FALSE && in_ml == FALSE && c == '/' && s[i+1] == '*') {
-        //in multiline comment
-        printf("mlstart, %d, in? %d, ml? %d\n", i, in_cmt, in_ml);
+    if (in_ml == FALSE) {
+      if (c == '/' && s[i+1] == '*') {
         ml_start = i;
-        printbuf(s, i);
-        in_ml == TRUE;
-      } else if (in_ml == TRUE && ml_start > -1 && c == '*' && s[i+1] == '/') { //check also if in_cmt is true?
-        ml_end = i + 1;
-        printf("mlend, %d, in? %d, ml? %d\n", i+1, in_cmt, in_ml);
-        printbuftail(s, ml_end, len);
-        in_ml == FALSE;
+        i++;
+        in_ml = TRUE;
       }
+      if (in_ml == FALSE)
+        printf("%c", c);
     } else {
-      //we are inside a comment
+      if (c == '*' && s[i+1] == '/') {
+        i++;
+        in_ml = FALSE;
+      } else {
+        if (ml_start > -1 && c == '\n') 
+          printf("\n");
+      }
     }
   }
 }
 
-void printbuf(char buf[], int end) {
-  int i;
-  for (i = 0; i < end; i++) 
-    putchar(buf[i]);
-  putchar('\n'); //DEBUG
-}
-
-void printbuftail(char buf[], int start, int len) {
-  int i;
-  for (i = start + 1; i < len; i++) 
-    putchar(buf[i]);
-}
 
 /* get_line: read a line into s, return length */
 int get_line(char s[], int lim) {

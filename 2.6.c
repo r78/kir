2f54d558e58a1f4cff6e29e824d0ed5bfe6729fa
r <r@localhost>
2016-04-17 18:16:14 +0200

    new file:   2.6.c

---
+++ 2.6.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+unsigned getbits(unsigned x, int p, int n);
+unsigned setbits(unsigned x, int p, int n, unsigned y);
+void DisplayBits(unsigned value);
+
+int main() {
+    DisplayBits(234);
+    DisplayBits(555);
+    printf("\n");
+    DisplayBits(setbits(234, 7, 4, 555));
+    return 0;
+}
+
+
+/* getbits: get n bits from position p */
+unsigned getbits(unsigned x, int p, int n) {
+  return (x >> (p+1-n)) & ~(~0 << n);
+}
+
+/* returns x with the n bits that begin at position p set 
+ * to the rightmost n bits of y , leaving the other bits unchanged. */
+unsigned setbits(unsigned x, int p, int n, unsigned y) {
+  //rightmost n bits of y
+  //y & ~(~0 << n);
+  return (x & ~(~(~0 << n) << (p+1-n))) | ((y & ~(~0 <<n))<< (p+1-n));
+}
+
+
+void DisplayBits(unsigned value) {
+    int i = 0, numbits = sizeof(unsigned int)*8;
+    for(;i<numbits;i++) {
+        if(!(i%4) && i)
+            printf(" ");
+        printf("%d", !(!(value & (1<<(numbits-i-1)))));
+    }
+    printf("\n");
+}
